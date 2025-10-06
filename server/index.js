571a85879bb9159427c60fdec03e2b534d0d7b0a
saadahmedsa <ss5099903@gmail.com>
2025-10-06 16:17:10 +0500

    user api added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,33 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 3000
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors"
+import connectDB from "./src/db/index.js";
+import cookieParser from "cookie-parser";
+import user from "./src/routes/userroute.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000", 
+  credentials: true,              
+}));
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello ams app !");
+});
+app.use("/api", user);
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!! ", err);
+  })
---
+++ server/src/models/User.js
@@ -0,0 +1,40 @@
+
+
+import bcrypt from "bcrypt";
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+},
+  { timestamps: true }
+);
+
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+export default mongoose.model("User", userSchema);
